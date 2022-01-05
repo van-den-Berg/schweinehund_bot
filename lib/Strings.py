2c80f5563c542719575ca6a82c03b02ef7a68ba2
Paul <paul.vdberg7@gmail.com>
2022-01-05 22:45:52 +0100

    Register process implemented. Missing: store the user data persistent

---
+++ lib/Strings.py
@@ -0,0 +1,17 @@
+class Help:
+    help_text = "this is a dummy text for the help section"
+    avail_commands = "die folgenden Commands sind verfügbar: ..."
+
+
+def registration_succesfull_private(name: str) -> str:
+    return "Hallo und Willkommen " + name + ", wir freuen uns dass du hier bist!"
+
+
+def registration_succesfull_group(name: str) -> str:
+    return "Woop Woop! Begrüßt " + name + " im Team!!"
+
+
+class Registration:
+    welcome_text = "Hey, schön dass du auch versuchst, deinen Arsch hoch zu bekommen! Damit alle wissen mit wem sie sich anlegen, fehllt noch dein Name. Mit diesem Namen wirst du in Zukunft von mir angesprochen und in Zusammenfassungen auftauchen. \n \n Wie darf ich dich nennen?"
+    first_need_to_open_chat = "Damit du mitmachen kannst, musst du mich bei dir starten. Gehe dazu auf mein Profil und beginne einen Chat. Anschließend kannst du hierher zurückkommen und dich registrieren."
+    already_registered = "Du bist bereits mit im Team!"
--- main.py
+++ main.py
@@ -8,50 +8,70 @@ import sys
 import json
 
 # import telegram_send
+from lib.Strings import registration_succesfull_group, registration_succesfull_private
 from models.Activity import Activity
-from services.FileServices import read_json
-from models import User
+from services import FileServices
+from telegram import message
+from models.User import User
 from services.MessageServices import get_sender_id
+from lib import Strings
 
-configJson = read_json('config.json')
+configJson = FileServices.read_json('config.json')
 
 bot = telebot.TeleBot(
     token=configJson["tel_api_token"])  # supply your future bot with the token you have received
-msg: list = []
-users: dict
+list_of_items_scrap: list = []
+users: dict[int, User] = {-1: User(uid=-1, calling_name="dummy")}
 
 
 @bot.message_handler(commands=['define', 'Define'])
-def echo_msg(message):
-    echo = bot.send_message(chat_id=message.chat.id,
-                            text='What word would you want me to extract, sir?')
+def echo_msg(msg: message):
+    echo = bot.send_message(chat_id=msg.chat.id,
+                            text='What word would you want me to save, sir?')
     bot.register_next_step_handler(message=echo, callback=extract_msg)
 
 
-def extract_msg(message):
-    msg.append(message.text)
-    print(msg)
+def extract_msg(msg: message):
+    list_of_items_scrap.append(msg.text)
+    print(list_of_items_scrap)
 
 
 @bot.message_handler(commands=['print'])
-def print_list(message):
-    bot.send_message(chat_id=message.chat.id,
-                     text=str(msg))
+def print_list(msg: message):
+    bot.send_message(chat_id=msg.chat.id,
+                     text=str(list_of_items_scrap))
 
 
-@bot.message_handler(commands=['printMessage'])  # PPrints the content of the sent message
-def save_to_json(message):
-    bot.send_message(chat_id=message.chat.id, text=str(message))
+@bot.message_handler(commands=['printMessage'])  # Prints the content of the sent message
+def print_message(msg: message):
+    bot.send_message(chat_id=msg.chat.id, text=str(msg))
 
 
-@bot.message_handler(commands=['printUserId'])  # PPrints the content of the sent message
-def save_to_json(message):
-    bot.send_message(chat_id=message.chat.id, text=str(get_sender_id(message)))
-# def add_user(user: User, chat_id):
-#    user_doc: dict = {}
-#    user_doc["chat_id"] = chat_id
-#    user_doc["username"] =
-#    dict[chat_id] = user_doc
+@bot.message_handler(commands=['printUserId'])  # prints the userId
+def print_user_id(msg: message):
+    bot.send_message(chat_id=msg.chat.id, text=str(get_sender_id(msg)))
+
+
+@bot.message_handler(commands=['register'])
+def register(msg: message):
+    if users is not None and get_sender_id(msg) in users:
+        bot.send_message(msg.from_user.id, Strings.Registration.already_registered)
+        return
+    else:
+        try:
+            echo = bot.send_message(msg.from_user.id, Strings.Registration.welcome_text)
+            bot.register_next_step_handler(message=echo, callback=register_user, group_chat_id=msg.chat.id)
+        except:
+            bot.send_message(msg.chat.id, Strings.Registration.first_need_to_open_chat)
+
+
+def register_user(msg: message, group_chat_id: int):
+    uid = get_sender_id(msg)
+    new_user: User = User(uid=uid, calling_name=msg.text)
+    users[uid] = new_user
+    bot.send_message(msg.chat.id, registration_succesfull_private(new_user.get_callname()))
+    bot.send_message(group_chat_id, registration_succesfull_group(new_user.get_callname()))
+    print(str(users))
 
 
 def main_loop():
