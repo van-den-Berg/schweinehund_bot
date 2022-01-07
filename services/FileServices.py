dbcdc7c338a6f8d64e60b2f6ca63780be19734fd
tim <tim.vandenberg@stud.uni-goettingen.de>
2022-01-07 21:24:34 +0100

    typo and better style for file opening

--- services/FileServices.py
+++ services/FileServices.py
@@ -1,14 +1,12 @@
 import json
 
 
-def read_json(file: str) -> dict:
-    file = open(file, 'r')
-    json_dict = json.load(file)
-    file.close()
+def read_json(file_path: str) -> dict:
+    with open(file_path, 'r') as read_file:
+        json_dict = json.load(read_file)
     return json_dict
 
 
-def save_json_overwrite(json_data: dict, file: str):
-    file = open(file, 'w')
-    json.dump(json_data, file)
-    file.close()
+def save_json_overwrite(json_data: dict, file_path: str):
+    with open(file_path, 'w') as write_file:
+        json.dump(json_data, write_file)
