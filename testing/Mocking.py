719106b2da19a34a7e4eba08e012e407e119a1bc
tim <tim.vandenberg@stud.uni-goettingen.de>
2022-01-07 21:33:02 +0100

    different file structure, gitignore

--- testing/Mocking.py
+++ testing/Mocking.py
@@ -5,4 +5,4 @@ from models.data_storage import Data
 def mock_userdata():
     data_object = Data.Data()
     data_object.add_user(Data.User("12344", "myFunnyUsername", "myPrivateChatId"))
-    services.FileServices.save_json_overwrite(data_object, "data/users.json")
+    services.FileServices.save_json_overwrite(data_object, "data.json")
