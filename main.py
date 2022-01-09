4dd3267289bc3d8cca189502eaef537ef7623504
tim <tim.vandenberg@stud.uni-goettingen.de>
2022-01-09 01:24:45 +0100

    after having everything in a class: turns out bot does not work in class structure (@bot... is the problem). default values like File loctions should be in the config-JSON. We should change that

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
-from enum import Enum
-from datetime import date
-import json
+import os.path
 
 import telebot  # importing pyTelegramBotAPI library
 import time
 import sys
-import json
-from typing import Dict
 
 # import telegram_send
 from lib.Strings import registration_succesfull_group, registration_succesfull_private
 from models.Activity import Activity
+from models.data_storage import Data
 from services import FileServices
 from telegram import message
 from models.TelegramUser import User
@@ -19,21 +16,35 @@ from services.MessageServices import get_sender_id
 from lib import Strings
 from testing import Mocking
 
-config_json_path = 'config.json'
-data_json_path = 'data.json'
+mocking = True
+
+config_json_path: str = 'config.json'
+data_json_path: str = 'data.json'
+mock_data_json_path: str = 'mock_data.json'
 
 config_dict = FileServices.read_json(config_json_path)
+bot = telebot.TeleBot(token=config_dict["tel_api_token"])
 
-bot = telebot.TeleBot(
-    token=config_dict["tel_api_token"])  # supply your future bot with the token you have received
 list_of_items_scrap: list = []
-users: Dict[int, User] = {-1: User(uid=-1, calling_name="dummy")}
+
+
+# get data
+if mocking:
+    data_obj = Mocking.mock_userdata()
+    data_json_path = mock_data_json_path
+    FileServices.save_json_overwrite(json_data=data_obj, file_path=data_json_path)
+else:
+    if os.path.isfile(data_json_path):
+        data_obj = FileServices.read_json(data_json_path)
+    else:
+        data_obj = Data.Data(users={}, groups={})
+        FileServices.save_json_overwrite(json_data=data_obj, file_path=data_json_path)
+
 
 
 @bot.message_handler(commands=["startBot"])
 def echo_msg(msg: message):
     echo = bot.send_message(chat_id=msg.chat.id, text='Not implemented yet')
-
 
 @bot.message_handler(commands=['define', 'Define'])
 def echo_msg(msg: message):
@@ -42,33 +53,28 @@ def echo_msg(msg: message):
                                  'What would you like to add?')
     bot.register_next_step_handler(message=echo, callback=extract_msg)
 
-
 def extract_msg(msg: message):
     list_of_items_scrap.append(msg.text)
     print(list_of_items_scrap)
-
 
 @bot.message_handler(commands=['printList'])
 def print_list(msg: message):
     bot.send_message(chat_id=msg.chat.id,
                      text=str(list_of_items_scrap))
 
-
 @bot.message_handler(commands=['printMessage'])  # Prints the content of the sent message
 def print_message(msg: message):
     bot.send_message(chat_id=msg.chat.id, text=str(msg))
-
 
 @bot.message_handler(commands=['printUserId'])  # prints the userId
 def print_user_id(msg: message):
     bot.send_message(chat_id=msg.chat.id, text=str(get_sender_id(msg)))
 
-
 @bot.message_handler(commands=['register'])
 def register(msg: message):
-    data_dict = FileServices.read_json(data_json_path)
+    # data_dict = FileServices.read_json(data_json_path)
     print(msg)
-    if get_sender_id(msg) in data_dict['users'].keys():  # User already registered
+    if get_sender_id(msg) in data_obj.users.keys():  # User already registered
         bot.send_message(msg.from_user.id, Strings.Registration.already_registered)
         return
     else:
@@ -78,20 +84,17 @@ def register(msg: message):
         except:
             bot.send_message(msg.chat.id, Strings.Registration.first_need_to_open_chat)
 
-
 def register_user(msg: message, group_chat_id: int):
-    data_dict = FileServices.read_json(data_json_path)
+    # data_dict = FileServices.read_json(data_json_path)
     uid = get_sender_id(msg)
     new_user: User = User(uid=uid, calling_name=msg.text)
-    users[uid] = new_user
+    data_obj.users[uid] = new_user
     bot.send_message(msg.chat.id, registration_succesfull_private(new_user.get_callname()))
 
     bot.send_message(group_chat_id, registration_succesfull_group(new_user.get_callname()))
-    print(str(users))
-
+    print(data_obj.users)
 
 def main_loop():
-    Mocking.mock_userdata()
     bot.polling(none_stop=True)
     while True:
         time.sleep(1)
--- testing/Mocking.py
+++ testing/Mocking.py
@@ -2,7 +2,8 @@ import services.FileServices
 from models.data_storage import Data
 
 
-def mock_userdata():
-    data_object = Data.Data()
-    data_object.add_user(Data.User("12344", "myFunnyUsername", "myPrivateChatId"))
+def mock_userdata() -> Data:
+    data_object = Data.Data(users={}, groups={})
+    data_object.add_user(Data.User(user_id="12344", tel_username="myFunnyUsername", private_chat_id="myPrivateChatId", active_groups=[]))
     services.FileServices.save_json_overwrite(data_object, "data.json")
+    return data_object
