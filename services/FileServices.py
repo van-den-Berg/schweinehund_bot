4fb112c9d9af5388d38c1fc68d1b4a9db3d91a51
tim <tim.vandenberg@stud.uni-goettingen.de>
2022-01-09 01:22:03 +0100

    jsonpickle can deal with objects, much easier.

--- services/FileServices.py
+++ services/FileServices.py
@@ -1,12 +1,12 @@
-import json
+import jsonpickle as jsonpickle
 
 
 def read_json(file_path: str) -> dict:
     with open(file_path, 'r') as read_file:
-        json_dict = json.load(read_file)
+        json_dict = jsonpickle.decode(read_file.read())
     return json_dict
 
 
-def save_json_overwrite(json_data: dict, file_path: str):
+def save_json_overwrite(json_data, file_path: str):
     with open(file_path, 'w') as write_file:
-        json.dump(json_data, write_file)
+        write_file.write(jsonpickle.encode(json_data))
