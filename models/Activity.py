242df9f34c8873f57890ba79681322866c3ecd88
tim <tim.vandenberg@stud.uni-goettingen.de>
2022-01-10 14:22:20 +0100

    ENUMS

--- models/Activity.py
+++ models/Activity.py
@@ -1,6 +1,9 @@
-from enum import Enum
+from enum import Enum, auto
 
 
 class Activity(Enum):
-    SPORT = 1
-    CHILL_EVENING = 2
+    SPORT = auto()
+    GOOD_EVENING = auto()
+    WENT_OUTSIDE = auto
+    PRODUCTIVE_BY_10 = auto
+    NO_MEDIA_DURING_WORK = auto
