3d127f00ccedd0cb14af14b8e9929292d60024e5
Paul <paul.vdberg7@gmail.com>
2022-01-12 18:36:17 +0100

    correct time getter

--- models/HabitEntry.py
+++ models/HabitEntry.py
@@ -1,5 +1,6 @@
 import dataclasses
 import datetime
+import time
 
 from models.Activity import Activity
 
@@ -8,4 +9,4 @@ from models.Activity import Activity
 class HabitEntry:
     user_id: str
     activity: Activity
-    date: str = datetime.date.isoformat()  # isoformated datetime. has to be a string in order to be serializable for JSON
+    date: str = str(time.time()) # isoformated datetime. has to be a string in order to be serializable for JSON
