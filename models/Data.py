d10e099f1d23d83c79b5add6599f7c7585662223
tim <tim.vandenberg@stud.uni-goettingen.de>
2022-01-15 16:42:28 +0100

    ToDos

--- models/Data.py
+++ models/Data.py
@@ -16,11 +16,6 @@ class Data:
     def add_user(self, new_user: User):
         self.users[new_user.user_id] = new_user
 
-    ## TODO: read this
-    # I don't think we need this one can just use data_object.users[user_id]
-    # def get_user(self, user_id: str) -> User:
-    #    return self.users[user_id]
-
     def remove_user(self, user_id: str):
         self.users.pop(user_id)
 
@@ -32,23 +27,17 @@ class Data:
             return success
         return False
 
-    # TODO: needs testing
+    # needs testing
     def add_group(self, new_group: Group):
         if new_group.group_id in self.groups.keys():
-            # TODO: throw error: "group already exists"
-            # I think a print should suffice, otherwise the whole bot stops.
             print(f"The group with id {new_group.group_id} does already exist. It wasn't changed.")
         else:
             self.groups[new_group.group_id] = new_group
             for user_id in new_group.all_users:
                 self.users[user_id].active_groups.add(new_group.group_id)
 
-    # TODO: Read this
-    # I don't think we need this, see above
-    # def get_group(self, group_id: str) -> Group.py:
-    #    return self.groups[group_id]
 
-    # TODO: need testing
+    # need testing
     # returning True if succeeded
     def user_join_group(self, user_id: str, group_id: str, chosen_username: str) -> bool:
         if (user_id in self.users.keys()) and (group_id in self.groups.keys()):
