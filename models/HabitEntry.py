583941745b1e1a0a3a066753d133a5f639b28dbc
tim <tim.vandenberg@stud.uni-goettingen.de>
2022-01-15 15:55:35 +0100

    manage dates (to str and back)

--- models/HabitEntry.py
+++ models/HabitEntry.py
@@ -1,6 +1,5 @@
 import dataclasses
 import datetime
-import time
 
 from models.Activity import Activity
 
@@ -9,14 +8,7 @@ from models.Activity import Activity
 class HabitEntry:
     user_id: str
     activity: Activity
-    date: str # isoformated datetime. has to be a string in order to be serializable for JSON
+    date: str = datetime.date.today().isoformat()  # isoformated datetime. has to be a string in order to be serializable for JSON
 
-    def __init__(self, user_id: str, activity: Activity, date: str = str(time.time())):
-        self.user_id = user_id
-        self.activity = activity
-        self.date = date
-
-    # TODO: get datetime.date object from the millis date string
     def get_date(self) -> datetime.date:
-        ret_date : datetime.date
-        return ret_date
+        return datetime.date.fromisoformat(self.date)
